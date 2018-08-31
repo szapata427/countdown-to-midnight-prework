de073371e98d0292d72672e93d37e7d2f132a032
Steven Zapata <szapata427@gmail.com>
2018-08-31 20:56:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+def countdown(number)
 
-def countdown
+  while 0 < number
+  
+  puts "#{number} SECOND(S)!"
+  number -= 1
+
+  break if 0 == number
+  
+end
+ return  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep (runtime)
+
+   while runtime > 0
+    runtime -= 1
+    sleep 1
+  end
+
 end
